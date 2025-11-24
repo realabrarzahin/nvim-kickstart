1417624ff214ca03e30341b04feeecc3dd19c583
Md Abrar Zahin Antor <realabrarzahin@gmail.com>
2025-11-24 22:01:57 +0600

    nothing

---
+++ templates/cp_template.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = long long;
+using u64 = unsigned long long;
+using u32 = unsigned;
+
+using u128 = unsigned __int128;
+using i128 = __int128;
+
+#define endl '\n'
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+}
+
+int main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
+
+/**********************************************************************************
+ * Solve a lot of problems. Build knowledge & Implementation. Not hard problems, not easy problems.
+ * Learn from others. Try to make implementation as short and clean as possible. Get multiple accepted.
+ * Don't try to learn lot of tools. Learn on the go.
+ * After getting accepted. Try to memorize from beginning, what you did, why you did.
+ * Be consistent with your implementation of an algorithm.
+**********************************************************************************/
